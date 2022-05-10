0c550d4fe3862b05e314dc3e9652c9c07a4e5ded
Pedro Luiz <pedrinho.apolinario@hotmail.com>
2022-05-10 20:06:18 -0300

    AJUSTES SELECIONA MAPA E NOME DA TELA

---
+++ MainView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Model;
+using WpfApp1.ViewModel;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        private MainViewModel ViewModel;
+        public MainView()
+        {
+            ViewModel = new MainViewModel();
+            DataContext = ViewModel;
+            InitializeComponent();
+            ComboMapas.ItemsSource = Enum.GetValues(typeof(Mapas)).Cast<Mapas>();
+        }
+
+        private void Pesquisar_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (ComboMapas.SelectedItem == null)
+            {
+                MessageBox.Show("NÃO HÁ DADOS");
+                return;
+            }
+
+            ViewModel.SelecionaMapa((Mapas)ComboMapas.SelectedItem);         
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+
+        }
+
+        internal void SelecionaMapa(Mapas mapa)
+        {
+            string nomeMapa = mapa.ToString();
+
+            switch (nomeMapa)
+            {
+                case "Haven":
+                    MessageBox.Show("Mapa bom para Chamber");
+                    break;
+
+                case "Icebox":
+                    MessageBox.Show("Mapa bom para viper");
+                    break;
+
+                case "Breeze":
+                    MessageBox.Show("Mapa bom para Jett");
+                    break;
+
+                case "Ascent":
+                    MessageBox.Show("Mapa bom para Sova");
+                    break;
+
+                case "Split":
+                    MessageBox.Show("Mapa bom para Reyna");
+                    break;
+
+                case "Bind":
+                    MessageBox.Show("Mapa bom para Brimstone");
+                    break;
+
+                case "Fracture":
+                    MessageBox.Show("Mapa bom para Neon");
+                    break;
+
+                default:
+                    MessageBox.Show("DADOS NÃO ENCONTRADO");
+                    break;
+            }
+        }
+    }
+}
