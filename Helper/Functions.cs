7511e1683cd29f553174f28e2b6ace066d424e55
Pedro Luiz <pedrinho.apolinario@hotmail.com>
2022-05-11 08:31:27 -0300

    AJUSTES ENUM MAPAS

---
+++ Helper/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ValorantPicks.Helper
+{
+    public static class Functions 
+    {
+        public static string Description(this Enum eEnum)
+        {
+            var tipo = eEnum.GetType();
+            var membro = tipo.GetMember(eEnum.ToString());
+            var atributo = membro[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return ((DescriptionAttribute)atributo.ElementAt(0)).Description;
+        }
+    }
+}
--- Model/Mapas.cs
+++ Model/Mapas.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace ValorantPicks.Model
 {
     public enum Mapas
     {
+        [Description("ASCENT")]
         Ascent,
+        [Description("SPLIT")]
         Split,
+        [Description("HAVEN")]
         Haven,
+        [Description("BIND")]
         Bind,
+        [Description("ICEBOX")]
         Icebox,
+        [Description("ASCENT")]
         Breeze,
+        [Description("FRACTURE")]
         Fracture,
     }
 }
