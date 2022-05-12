e0ccda9624860512c7bf40b37064e4b9f5bdd63b
Pedro Luiz <pedrinho.apolinario@hotmail.com>
2022-05-12 08:51:10 -0300

    AJUSTADO BINDING ENUM NO XAML

---
+++ Helper/EnumBindingSourceExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace ValorantPicks.Helper
+{
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        private Type _enumType;
+
+        public Type EnumType
+        {
+            get => _enumType;
+            set
+            {
+                if (value == _enumType)
+                    return;
+                if (value != null)
+                {
+                    var enumType = Nullable.GetUnderlyingType(value) ?? value;
+                    if (!enumType.IsEnum)
+                        throw new ArgumentException("Argumento invalido, aceito somente Enum");
+                }
+                _enumType = value;
+            }
+        }
+        public EnumBindingSourceExtension() { }
+
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            EnumType = enumType;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (_enumType is null)
+                throw new InvalidOperationException("O Tipo do Enum precisa ser especificado.");
+
+            var actualEnumType = Nullable.GetUnderlyingType(_enumType) ?? _enumType;
+            var enumValues = Enum.GetValues(actualEnumType);
+
+            if (actualEnumType == _enumType)
+                return enumValues;
+
+            var tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
+            enumValues.CopyTo(tempArray, 1);
+            return tempArray;
+        }
+    }
+}
